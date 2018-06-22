1aeca70734b3a5304f94f9890520660153ccf28c
Alice Lieutier <alice@lieutier.me>
2018-06-22 17:02:53 +0100

    classes

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,71 +1,5 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            special_items = {
-                "Aged Brie": self.aged_brie_decay,
-                "Backstage passes to a TAFKAL80ETC concert": self.backstage_passes_decay,
-                "Sulfuras, Hand of Ragnaros": (lambda x : x),
-            }
-            if item.name in special_items:
-                special_items[item.name](item)
-            else:
-                self.typical_decay(item)
-            pass
-
-        
-    def typical_decay(self, item):
-        self.update_sell_in(item)
-        if item.sell_in >= 0:
-            item.quality -=1
-        else:
-            item.quality -= 2
-        self.readjust_quality_in_bounds(item)
-
-    def aged_brie_decay(self, item):
-        self.update_sell_in(item)
-        if item.sell_in >= 0:
-            item.quality +=1
-        else:
-            item.quality +=2
-        self.readjust_quality_in_bounds(item)
-
-    def backstage_passes_decay(self, item):
-        self.update_sell_in(item)
-        if item.sell_in >= 0:
-            item.quality += self.backstage_passes_increase_rate_for_sell_in(item.sell_in)
-        else: # concert has passed
-            item.quality = 0
-        self.readjust_quality_in_bounds(item)
-
-    def backstage_passes_increase_rate_for_sell_in(self, sell_in):
-        ## hacky one-liner:
-        # return 3 - min(10, sell_in) / 5
-        if sell_in >= 10:
-            return 1
-        if sell_in in range(5, 10):
-            return 2
-        if sell_in in range(0, 5):
-            return 3
-
-    # TODO implement conjured items
-    def conjured_item_decay(self, item):
-        self.typical_decay(item)
-
-
-    def update_sell_in(self, item):
-        item.sell_in -= 1
-        self
-
-    def readjust_quality_in_bounds(self, item):
-        item.quality = max(0, item.quality)
-        item.quality = min(50, item.quality)
-
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -74,4 +8,85 @@ class Item:
         self.quality = quality
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+        return "%s: %s, %s" % (self.__class__.__name__, self.sell_in, self.quality)
+
+class GildedRose(object):
+    def __init__(self, items):
+        self.items = [self.wrapItemInClass(item) for item in items]
+
+    def update_quality(self):
+        for item in self.items:
+            item.update()
+
+    def wrapItemInClass(self, item):
+        special_item_classes = {
+            "Aged Brie": AgedBrie,
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePasses,
+            "Sulfuras, Hand of Ragnaros": Sulfuras,
+        }
+        if item.name in special_item_classes:
+            return special_item_classes[item.name](item.name, item.sell_in, item.quality)
+        else:
+            return TypicalItem(item.name, item.sell_in, item.quality)
+
+class TypicalItem(Item):
+    def _update_sell_in(self):
+        self.sell_in -= 1
+        return self
+
+    def _update_quality(self):
+        if self.sell_in >= 0:
+            self.quality -=1
+        else:
+            self.quality -=2
+        return self
+
+    def _readjust_quality_in_bounds(self):
+        self.quality = max(0, self.quality)
+        self.quality = min(50, self.quality)
+        return self
+
+    def update(self):
+        # print(self.__methods__())
+        self._update_sell_in()
+        self._update_quality()
+        self._readjust_quality_in_bounds()
+        return
+
+
+class AgedBrie(TypicalItem):
+    def _update_quality(self):
+        if self.sell_in >= 0:
+            self.quality +=1
+        else:
+            self.quality +=2
+        return self
+
+class Sulfuras(Item):
+    def update(self):
+        pass
+
+class BackstagePasses(TypicalItem):
+    def _update_quality(self):
+        if self.sell_in >= 0:
+            self.quality += self.__backstage_passes_increase_rate_for_sell_in(self.sell_in)
+        else: # concert has passed
+            self.quality = 0
+
+    @staticmethod
+    def __backstage_passes_increase_rate_for_sell_in(sell_in):
+        ## hacky one-liner:
+        return 3 - min(10, sell_in) / 5
+        # if sell_in >= 10:
+        #     return 1
+        # if sell_in in range(5, 10):
+        #     return 2
+        # if sell_in in range(0, 5):
+        #     return 3
+
+if __name__ == '__main__':
+    items = [Item("Aged Brie", 1, 48)]
+    gilded_rose = GildedRose(items)
+    for i in range(1, 5):
+        print(gilded_rose.items)
+        gilded_rose.update_quality()
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -10,64 +10,64 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 10, 10)]
         gilded_rose = GildedRose(items)
         expected_decay = [(9, 9), (8, 8), (7, 7), (6, 6)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     def test_end_of_sellin(self):
         items = [Item("foo", 2, 10)]
         gilded_rose = GildedRose(items)
         # decay is two times faster after sell-in date
         expected_decay = [(1, 9), (0, 8), (-1, 6), (-2, 4), (-3, 2)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     def test_end_of_sellin_and_quality_0(self):
         items = [Item("foo", 1, 2)]
         gilded_rose = GildedRose(items)
         expected_decay = [(0, 1), (-1, 0), (-2, 0)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     def test_min_quality_0(self):
         items = [Item("foo", 10, 2)]
         gilded_rose = GildedRose(items)
         expected_decay = [(9, 1), (8, 0), (7, 0), (6, 0), (5, 0)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     # defensive testing
     def test_quality_negative(self):
         items = [Item("foo", 10, -10)]
         gilded_rose = GildedRose(items)
         expected_decay = [(9, 0)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     def test_quality_more_than_50(self):
         items = [Item("foo", 10, 200)]
         gilded_rose = GildedRose(items)
         expected_decay = [(9, 50)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     # test special items
     def test_aged_brie(self):
         items = [Item("Aged Brie", 2, 0)]
         gilded_rose = GildedRose(items)
         expected_decay = [(1, 1), (0, 2), (-1, 4), (-2, 6)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     def test_aged_brie_max_quality(self):
         items = [Item("Aged Brie", 1, 48)]
         gilded_rose = GildedRose(items)
         expected_decay = [(0, 49), (-1, 50), (-2, 50)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     def test_sulfuras(self):
         items = [Item("Sulfuras, Hand of Ragnaros", 4, 200)]
         gilded_rose = GildedRose(items)
         expected_decay = [(4, 200), (4, 200), (4, 200)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     def test_sulfuras(self):
         items = [Item("Sulfuras, Hand of Ragnaros", -4, 200)]
         gilded_rose = GildedRose(items)
         expected_decay = [(-4, 200), (-4, 200), (-4, 200)]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     def test_backstage_passes(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 12, 10)]
@@ -78,18 +78,18 @@ class GildedRoseTest(unittest.TestCase):
             (4, 25), (3, 28), (2, 31), (1, 34), (0, 37),
             (-1, 0), (-2, 0)
         ]
-        self.assertDecayOverNDays(gilded_rose, items[0], expected_decay)
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], expected_decay)
 
     def test_backstage_passes_max_quality(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 49)]
         gilded_rose = GildedRose(items)
-        self.assertDecayOverNDays(gilded_rose, items[0], [(14, 50)])
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], [(14, 50)])
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)]
         gilded_rose = GildedRose(items)
-        self.assertDecayOverNDays(gilded_rose, items[0], [(9, 50)])
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], [(9, 50)])
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
         gilded_rose = GildedRose(items)
-        self.assertDecayOverNDays(gilded_rose, items[0], [(4, 50)])
+        self.assertDecayOverNDays(gilded_rose, gilded_rose.items[0], [(4, 50)])
 
     # decay expects an array of pairs on expected sell-in and quality value over the next n days
     def assertDecayOverNDays(self, gilded_rose, item, expected_decay):
@@ -101,12 +101,12 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEquals(
             item.sell_in,
             expected_sellin,
-            'incorrect sell_in value {}, expected {}'.format(item.sell_in, expected_sellin)
+            '{}: incorrect sell_in value {}, expected {}'.format(item, item.sell_in, expected_sellin)
         )
         self.assertEquals(
             item.quality,
             expected_quality,
-            'incorrect quality value {}, expected {}'.format(item.quality, expected_quality)
+            '{}: incorrect quality value {}, expected {}'.format(item, item.quality, expected_quality)
         )
 
 if __name__ == '__main__':
